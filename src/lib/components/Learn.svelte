ecfcec01cd0e6224ac9a909d6178dbf7471ff3b9
Nicoletta Alba <123635180+usualalteration@users.noreply.github.com>
2025-02-26 21:43:23 +0100

    Seconda sezione aggiornata

---
+++ src/lib/components/Learn.svelte
@@ -0,0 +1,57 @@
+<script>
+    import "$lib/styles/learn-styles.css";
+    export let title = "LEARN NUVOLARIS";
+    export let subtitle = "Nuvolaris is extremely easy to use: in a week you will be able to master it!";
+</script>
+
+<section class="learn-nuvolaris">
+    <div class="container">
+        <!-- Testo e informazioni -->
+        <div class="text-content">
+            <h2><span>{title.split(" ")[0]}</span> {title.split(" ")[1]}</h2>
+            <p class="subtitle">{subtitle}</p>
+
+            <div class="description">
+                <h3>What it can do</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
+                </p>
+            </div>
+
+            <!-- Pulsanti -->
+            <div class="buttons">
+                <div class="btn-box">
+                    <p class="btn-title">Use Cases</p>
+                    <a href="#" class="btn">WATCH THEM NOW</a>
+                </div>
+                <div class="btn-box">
+                    <p class="btn-title">Our Tutorials</p>
+                    <a href="#" class="btn">LEARN NOW</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Immagine di sfondo -->
+        <div class="background-image"></div>
+    </div>
+
+    <!-- Seconda sezione: Compatibility -->
+    <div class="compatibility">
+        <div class="compatibility-content">
+            <h2>COMPAT <span>IBILITY</span></h2>
+            <p>Programming languages supported by nuvolaris</p>
+            <div class="icons">
+                <img src="img/icons/ruby.png" alt="Ruby">
+                <img src="img/icons/python.png" alt="Python">
+                <img src="img/icons/php.png" alt="PHP">
+                <img src="img/icons/go.png" alt="Go">
+                <img src="img/icons/node.png" alt="Node.js">
+                <img src="img/icons/swift.png" alt="Swift">
+                <img src="img/icons/java.png" alt="Java">
+            </div>
+        </div>
+
+        <!-- Immagine uomo con occhiali -->
+        <div class="developer-image"></div>
+    </div>
+</section>
--- src/lib/components/Products.svelte
+++ src/lib/components/Products.svelte
@@ -7,30 +7,32 @@
       { title: "PRODUCTION", category: "NUVOLARIS", image: "img/icona-enterprise.png", color: "#E76F51" },
       { title: "ON PREMISES", category: "NUVOLARIS", image: "img/Livello_1.png", color: "#0077B6" }
     ];
+
+    
 </script>
 
 <section class="products-section">
 
         <!-- Titolo e descrizione -->
         <div class="text-section">
-        <div class="sidebar"></div>
-        <img class="image-container" src="../img/design-scritta-home.png"/>
-        
-        <p class="highlighted-text">We make Kubernetes a serverless solution</p>
-        <p class="highlighted-text2">Choose the best version of nuvolaris according to your business</p>
-        <div class="three-columns">
-            <div class="column left">
-                <pre class="text1">3
-                    PROD
-                    UCTS</pre>
-                <pre class="text2">
-                    Customize your
-                    solution to better fit
-                    your company</pre>
-                <img src="../img/monitor-why-choose 1.png"/>
-            </div>
+            <img class="image-container" src="../img/design-scritta-home.png"/>
+            
+            <p class="highlighted-text">We make Kubernetes a serverless solution</p>
+            <p class="highlighted-text2">Choose the best version of nuvolaris according to your business</p>
+            <div class="three-columns">
+<div class="column left">
+    <img class="sidebar" src="../img/SIdebar.png">
+    <!-- <pre class="text1">3
+        PROD
+        UCTS</pre>
+        <pre class="text2">
+            Customize your
+            solution to better fit
+            your company</pre>
+            <img src="../img/monitor-why-choose 1.png"/> -->
+        </div> 
             <div class="product-grid">
-                <div class="product-card">
+                <div id="development" class="product-card">
                     <h3>NUVOLARIS DEVELOPMENT</h3>
                     <img src="img/icona-monitor.png" alt="Development">
                     <a href="#" class="btn">LEARN MORE</a>
