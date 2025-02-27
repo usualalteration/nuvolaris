d4cd82dc494bd8c16f1dce76f46c4afd62de4cf3
Nicoletta Alba <123635180+usualalteration@users.noreply.github.com>
2025-02-27 10:42:20 +0100

    Seconda sezione aggiornata

--- src/lib/components/Benefits.svelte
+++ src/lib/components/Benefits.svelte
@@ -1,32 +1,41 @@
 <script>
-     import "$lib/styles/product-styles.css";
+    import "$lib/styles/benefits-styles.css";
 </script>
+
 <section class="benefits-section">
-    <h2>Benefits</h2>
-    <h3>WHY CHOOSE <span>NUVOLARIS</span></h3>
+    <!-- Colonna 1 vuota -->
+    <div class="empty-column"></div>
+
+    <!-- Colonna 2 (Titoli) -->
+    <div class="benefits-text">
+        <h2>Benefits</h2>
+        <h3>WHY CHOOSE <span>NUVOLARIS</span></h3>
+    </div>
+
+    <!-- Colonna 3 (Benefit) -->
     <div class="benefits-container">
         <div class="benefit">
-            <h4>1. COST REDUCTION</h4>
-            <p>
-                ✔ Reduced development time<br>
-                ✔ Fewer people needed<br>
-                ✔ Less expertise required
-            </p>
+            <img src="./img/numerazione 1.png" alt="Cost Reduction">
+            <div>
+                <h4>COST REDUCTION</h4>
+                <p>✔ Reduced development time<br>✔ Fewer people needed<br>✔ Less expertise required</p>
+            </div>
         </div>
+
         <div class="benefit">
-            <h4>2. SIMPLICITY</h4>
-            <p>
-                ✔ Deploy applications easily<br>
-                ✔ No need for code migration
-            </p>
+            <img src="./img/numerazione 2.png" alt="Simplicity">
+            <div>
+                <h4>SIMPLICITY</h4>
+                <p>✔ Deploy applications easily<br>✔ No need for code migration</p>
+            </div>
         </div>
+
         <div class="benefit">
-            <h4>3. PORTABILITY</h4>
-            <p>
-                ✔ Kubernetes compatible<br>
-                ✔ Easy to learn<br>
-                ✔ Open to all developers
-            </p>
+            <img src="./img/numerazione 3.png" alt="Portability">
+            <div>
+                <h4>PORTABILITY</h4>
+                <p>✔ Kubernetes compatible<br>✔ Easy to learn<br>✔ Open to all developers</p>
+            </div>
         </div>
     </div>
 </section>
