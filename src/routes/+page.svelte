ad156de25e8942369dbd80d31282c71062332e44
Nicoletta Alba <123635180+usualalteration@users.noreply.github.com>
2025-03-08 14:47:26 +0100

    Sistemato il passaggio da una pagina all'altra

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,17 @@
   import Compatibility from "$lib/components/1-Homepage/Compatibility.svelte";
   import ContactForm from "$lib/components/1-Homepage/Contact.svelte";
   import Footer from "$lib/components/1-Homepage/Footer.svelte";
+
   import DevelopmentHero from "$lib/components/2-Development/1-DevelopmentHero.svelte";
   import DevelopmentDiscover from "$lib/components/2-Development/3-DevelopmentDiscover.svelte";
   import DevelopmentChoose from "$lib/components/2-Development/2-DevelopmentChoose.svelte";
   import DevelopmentProducts from "$lib/components/2-Development/4-DevelopmentProducts.svelte";
+
   import ProductionHero from "$lib/components/4-Production/1-ProductionHero.svelte";
   import ProductionDiscover from "$lib/components/4-Production/3-ProductionDiscover.svelte";
   import ProductionChoose from "$lib/components/4-Production/2-ProductionChoose.svelte";
   import ProductionProducts from "$lib/components/4-Production/4-ProductionProducts.svelte";
+
   import OnPremisesHero from "$lib/components/3-OnPremises/1-OnPremisesHero.svelte";
   import OnPremisesDiscover from "$lib/components/3-OnPremises/3-OnPremisesDiscover.svelte";
   import OnPremisesChoose from "$lib/components/3-OnPremises/2-OnPremisesChoose.svelte";
@@ -21,57 +24,50 @@
 
   let currentPage = "homepage";
 
-  function switchToDevelopment() {
-  console.log("Switching to development");
-  currentPage = "development";
-}
-
-function switchToProduction() {
-  console.log("Switching to production");
-  currentPage = "production";
-}
-
-function switchToOnPremises() {
-  console.log("Switching to on premises");
-  currentPage = "onpremises";
-}
-
+  function switchPage(page) {
+    console.log(`Switching to ${page}`);
+    currentPage = page;
+  }
 </script>
 
-<main>
-  {#if currentPage === "development"}
-    <Navbar />
-    <DevelopmentHero />
-    <DevelopmentChoose />
-    <DevelopmentDiscover />
-    <DevelopmentProducts />
-  {:else if currentPage === "production"}
-    <Navbar />
-    <ProductionHero />
-    <ProductionChoose />
-    <ProductionDiscover />
-    <ProductionProducts />
-  {:else if currentPage === "onpremises"}
-    <Navbar />
-    <OnPremisesHero />
-    <OnPremisesChoose />
-    <OnPremisesDiscover />
-    <OnPremisesProducts />
-  {:else}
-    <Navbar />
-    <HeroSection />
-    <Products on:switchToDevelopment={switchToDevelopment} on:switchToProduction={switchToProduction} on:switchToOnPremises={switchToOnPremises} />
-    <Learn />
-    <Compatibility />
-  {/if}
-  <ContactForm />
-  <Footer />
-</main>
+  <main>
+    <Navbar /> <!-- Navbar è sempre presente, quindi sta fuori dal blocco if -->
+  
+    {#if currentPage === "development"}
+      <DevelopmentHero />
+      <DevelopmentChoose />
+      <DevelopmentDiscover />
+      <DevelopmentProducts />
+    {:else if currentPage === "production"}
+      <ProductionHero />
+      <ProductionChoose />
+      <ProductionDiscover />
+      <ProductionProducts />
+    {:else if currentPage === "onpremises"}
+      <OnPremisesHero />
+      <OnPremisesChoose />
+      <OnPremisesDiscover />
+      <OnPremisesProducts />
+    {:else}
+      <HeroSection />
+      <Products 
+        on:switchToDevelopment={() => switchPage("development")}
+        on:switchToProduction={() => switchPage("production")}
+        on:switchToOnPremises={() => switchPage("onpremises")}
+      />
+      <Learn />
+      <Compatibility />
+    {/if}
+  
+    <ContactForm />
+    <Footer />
+  </main>
 
 <style>
   @import "tailwindcss/base";
   @import "tailwindcss/components";
   @import "tailwindcss/utilities";
+  
   main {
     font-family: "Inter", sans-serif;
     color: #333;
@@ -80,5 +76,4 @@ function switchToOnPremises() {
     padding: 0;
     overflow-x: hidden;
   }
-  
 </style>
