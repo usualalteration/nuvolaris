506edff429b353875eae36de692c3497c70576b1
Nicoletta Alba <123635180+usualalteration@users.noreply.github.com>
2025-03-17 16:31:58 +0100

    Realizzate altre sezioni

--- src/lib/components/2-Development/4-DevelopmentDiscover.svelte
+++ src/lib/components/2-Development/4-DevelopmentDiscover.svelte
@@ -3,37 +3,40 @@
   import "$lib/styles/global-font-styles.css";
   import "$lib/styles/Development-styles/discover-styles.css";
 </script>
-<section class=half-height-section>
-<div class="flex-wrapper">
-<div class="flex-container">
-  <div class=flex1></div>
-  <!-- Box Immagine -->
-  <div class="flex-1 image-container">
-    <img src="/img/mug.svg" alt="Mug" class="mug" />
-  </div>
 
-  <!-- Box Testo -->
-  <div class="flex-2 text-container">
-    <h1>DISCOVER <br> NUVOLARIS <br> <span>PRODUCTS</span></h1>
-  </div>
+<section class="half-height-section">
+  <div class="flex-wrapper">
+    <div class="flex-container">
+      <div class="flex-8"></div>
+      <div class="flex-1 image-container">
+        <img src="/img/mug.svg" alt="Mug" class="mug" />
+      </div>
 
-  <!-- Box Pulsanti -->
-  <div class="flex-1 buttons-container">
-    <div class="button-group">
-      <div class="label">Development <span>Server</span></div>
-      <button>LEARN MORE</button>
-    </div>
-    <div class="button-group">
-      <div class="label">Production <span>Server</span></div>
-      <button>LEARN MORE</button>
-    </div>
-    <div class="button-group">
-      <div class="label">On <span>Premises</span></div>
-      <button>LEARN MORE</button>
+      <!-- Box Testo -->
+      <div class="flex-2" style="margin-right:10%;">
+        <h2 class="letters" id="white" style="text-align:right;">DISCOVER <br> NUVOLARIS <br> </h2>
+          <h3 class="verybigletters bold" id="white">PROD</h3>
+          <br>
+          <h3 class="verybigletters bold" id="white">&nbsp;UCTS</h3>
+      </div>
+
+      <!-- Box Pulsanti -->
+      <div class="flex-1 buttons-container">
+        <div class="button-group">
+          <div class="label">Development <span>Server</span></div>
+          <button class="btn">LEARN MORE</button>
+        </div>
+        <div class="button-group">
+          <div class="label">Production <span>Server</span></div>
+          <button class="btn">LEARN MORE</button>
+        </div>
+        <div class="button-group">
+          <div class="label">On <span>Premises</span></div>
+          <button class="btn">LEARN MORE</button>
+        </div>
+      </div>
+      <div class="flex-8"></div>
+
     </div>
   </div>
-  <div class=flex1></div>
-
-</div>
-</div>
 </section>
