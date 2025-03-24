12a986eeea1508f9a736ec1772814a6955b0323d
Nicoletta Alba <123635180+usualalteration@users.noreply.github.com>
2025-03-24 23:03:34 +0100

    Ho quasi finito la versione desktop di tutto il sito! manca 1 sezione

--- src/lib/components/2-Development/4-DevelopmentDiscover.svelte
+++ src/lib/components/2-Development/4-DevelopmentDiscover.svelte
@@ -8,17 +8,14 @@
   <div class="flex-wrapper">
     <div class="flex-container">
       <div class="flex-8"></div>
-      <div class="flex-1 image-container">
-        <img src="/img/mug.svg" alt="Mug" class="mug" />
-      </div>
 
       <!-- Box Testo -->
-      <div class="flex-2" style="margin-right:10%;">
+      <!-- <div class="flex-2" style="margin-right:10%;">
         <h2 class="letters" id="white" style="text-align:right;">DISCOVER <br> NUVOLARIS <br> </h2>
           <h3 class="verybigletters bold" id="white">PROD</h3>
           <br>
           <h3 class="verybigletters bold" id="white">&nbsp;UCTS</h3>
-      </div>
+      </div> -->
 
       <!-- Box Pulsanti -->
       <div class="flex-1 buttons-container">
