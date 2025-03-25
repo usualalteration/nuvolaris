df6fefb37323075a3a6c4e502f55003535afee9b
Nicoletta Alba <123635180+usualalteration@users.noreply.github.com>
2025-03-25 16:17:00 +0100

    Finita versione desktop

--- src/lib/components/1-Homepage/3-Learn.svelte
+++ src/lib/components/1-Homepage/3-Learn.svelte
@@ -3,49 +3,54 @@
   import "$lib/styles/global-font-styles.css";
   import "$lib/styles/global-styles.css";
 </script>
+
 <main>
-<section class="learn">
-  <div class="grid-container2">
-    <!-- Celle unite -->
-    <div class="grid-item item-1">
-      <div class="center-text">
-        <h3 id="white" class=" white verybigletters">
-          L<span>E</span>A<span>RN</span>
-        </h3>
-        <h3 id="white" class="white letters">NUVOLARIS</h3>
+  <section class="learn">
+    <div class="grid-container2">
+
+      <!-- Colonna 1 (Sinistra) -->
+      <div class="grid-item item-1">
+        <div class="center-text">
+          <h3 class="white verybigletters">
+            L<span>E</span>A<span>RN</span>
+          </h3>
+          <h3 class="white letters">NUVOLARIS</h3>
+        </div>
+        <br />
+        <p>
+          Nuvolaris is <strong>extremely</strong> easy to use:
+          in a week you will be able to master it!
+        </p>
       </div>
-      <br />
-      <p id="white" class="miniletters right-text right">Nuvolaris is <span>&nbsp;extremely&nbsp;</span></p>
-      <p id="white" class="miniletters right-text right"><span>&nbsp;easy to use&nbsp;</span>: in a week</p>
-      <p id="white" class="miniletters right-text right">you will be able to</p>
-      <p id="white" class="miniletters right-text right">master it!</p>
-    </div>
-    <div class="grid-item item-2">
-      <h3>What it can do</h3>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisqua.
-      </p>
-    </div>
-    <div class="grid-item item-3">
-    <div class="transparent-box">
+
+      <!-- Colonna 2 (Centrale) -->
+      <div class="grid-item item-2">
+        <h3>What it can do</h3>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit
+          voluptatem accusantium doloremque laudantium...
+        </p>
+      </div>
+
+      <!-- Colonna 3 (Destra) -->
+      <div class="grid-item item-3">
         <div class="buttons-container">
-          <div class="buttons">
-            <h3>Use Cases</h3>
-            <button class="btn">WATCH THEM NOW</button>
-          </div>
           <div>
-            <h3>Our Tutorials</h3>
-            <button class="btn">LEARN NOW</button>
+            <div class="transparent-box">
+              <div class="buttons-container">
+                <h3>Use Cases</h3>
+                <button class="btn">WATCH THEM NOW</button>
+              </div>
+              <br />
+              <div class="buttons-container">
+                <h3>Our Tutorials</h3>
+                <button class="btn">LEARN NOW</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="grid-item item-4"><!-- NUOVA SEZIONE --></div>
+
     </div>
-  </div>
-</section>
+  </section>
 </main>
