28d19ddd2f029876d1225fd4aa9079547a759656
Nicoletta Alba <123635180+usualalteration@users.noreply.github.com>
2025-03-26 17:01:58 +0100

    Finita versione desktop

--- src/lib/components/2-Development/4-DevelopmentDiscover.svelte
+++ src/lib/components/2-Development/4-DevelopmentDiscover.svelte
@@ -8,8 +8,35 @@
   <div class="flex-wrapper">
     <div class="flex-container">
       <div class="flex-8"></div>
-
-      <!-- Box Testo -->
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<!-- Box Testo -->
       <!-- <div class="flex-2" style="margin-right:10%;">
         <h2 class="letters" id="white" style="text-align:right;">DISCOVER <br> NUVOLARIS <br> </h2>
           <h3 class="verybigletters bold" id="white">PROD</h3>
