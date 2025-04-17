bbbc571dafbc3c2d0f8678970643329aa39786f2
Nicoletta Alba <123635180+usualalteration@users.noreply.github.com>
2025-04-17 19:49:35 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'web',
-			assets: 'web',
-			fallback: 'index.html', // solo se vuoi SPA
-			precompress: false,
-			strict: true
-		}),
-		files: {
-			assets: 'web'
-		},
-		prerender: {
-			entries: ['*'] // renderizza tutte le rotte staticamente
-		}
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
