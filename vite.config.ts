6195416e2a7abc48e16d2fa2dc68a569efb1634b
usualalteration <usualalteration@gmail.com>
2025-04-22 17:53:31 +0200

    Rimossa riga bianca sopra hero homepage

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 8080,
+		proxy: {
+			"/api/my": {
+				target: process.env.NUVDEV_HOST,
+				changeOrigin: true
+			}
+		}
+	}
 });
